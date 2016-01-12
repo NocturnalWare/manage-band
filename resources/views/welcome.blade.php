9e5d03c72f4648c82c9fa84ec8ee6e5a32c5f761
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-12 00:14:40 -0500

    Corrects an alignment issue.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -20,28 +20,28 @@
                     <form action="{{route('signUp')}}" method="post">
                         <input type="hidden" name="_token" value="{{csrf_token()}}" />
                         <span class="row">
-                            <label class="col-sm-3" style="color:#fff">First Name </label>
-                            <input class="col-sm-9 pull-right" name="first_name" />
+                            <label class="col-sm-4" style="text-align:right;color:#fff">First Name </label>
+                            <input class="col-sm-8 pull-right" name="first_name" />
                         </span>
                         
                         <span class="row">
-                            <label class="col-sm-3" style="color:#fff">Last Name</label>
-                            <input class="col-sm-9 pull-right" name="last_name" />
+                            <label class="col-sm-4" style="text-align:right;color:#fff">Last Name</label>
+                            <input class="col-sm-8 pull-right" name="last_name" />
                         </span>
                         
                         <span class="row">
-                            <label class="col-sm-3" style="color:#fff">Email</label>
-                            <input class="col-sm-9 pull-right" name="email" placeholder="email@address.com" />
+                            <label class="col-sm-4" style="text-align:right;color:#fff">Email</label>
+                            <input class="col-sm-8 pull-right" name="email" placeholder="email@address.com" />
                         </span>
                         
                         <span class="row">
-                            <label class="col-sm-3" style="color:#fff">Zip Code</label>
-                            <input class="col-sm-9 pull-right" name="zip"></span>
+                            <label class="col-sm-4" style="text-align:right;color:#fff">Zip Code</label>
+                            <input class="col-sm-8 pull-right" name="zip"></span>
                         </span>
 
                         <span class="row">
-                            <label class="col-sm-3" style="color:#fff">I'm a </label>
-                            <select class="col-sm-9 pull-right" style="color:#000;">
+                            <label class="col-sm-4" style="text-align:right;color:#fff">I'm a </label>
+                            <select class="col-sm-8 pull-right" style="color:#000;">
                                 <option value="band">Band</option>
                                 <option value="venue">Venue</option>
                                 <option value="promoter">Promoter</option>
