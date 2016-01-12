86c666789aee6723249d439b10cef2ece667e8e7
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-11 21:11:50 -0500

    Optimizes for mobile

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -16,25 +16,25 @@
                 </form>
             </div>
             <div class="content col-sm-12" style="background-color:#000;font-size:1.5em;">
-                <div class="img-responsive row" style="background-image: url('http://manage.band/images/groupshot.jpg');background-repeat:no-repeat;height:500px;">
+                <div class="img-responsive row" style="background-image: url('http://manage.band/images/groupshot.jpg');background-repeat:no-repeat;max-height:500px;">
                     <div class="col-sm-6 pull-right" style="background-color:#000;color:#fff;height:500px;padding-top:15%;opacity:.8;font-size:1em">
                         Manage your business from the studio, the road, or anywhere else that has an internet connection. <br> <br>
                     </div>
                 </div>
 
-                <div class="img-responsive row" style="background-image: url('http://manage.band/images/frontshot.jpg');background-repeat:no-repeat;height:500px;">
+                <div class="img-responsive row" style="background-image: url('http://manage.band/images/frontshot.jpg');background-repeat:no-repeat;max-height:500px;">
                     <div class="col-sm-6 pull-left" style="background-color:#000;color:#fff;height:500px;padding-top:15%;opacity:.8;font-size:1em">
                         From amps to Zildjian, manage your gear and make sure you have everything you need to succeed. <br> <br>
                     </div>
                 </div>
 
-                <div class="img-responsive row" style="background-image: url('http://manage.band/images/showshot.jpg');background-repeat:no-repeat;height:500px;">
+                <div class="img-responsive row" style="background-image: url('http://manage.band/images/showshot.jpg');background-repeat:no-repeat;max-height:500px;">
                     <div class="col-sm-6 pull-right" style="background-color:#000;color:#fff;height:500px;padding-top:15%;opacity:.8;font-size:1em">
                         Connect with the people you need and social media to quickly set up a show or tour. <br> <br>
                     </div>
                 </div>
 
-                <div class="img-responsive row" style="background-image: url('http://manage.band/images/merchshot.jpg');background-repeat:no-repeat;height:500px;">
+                <div class="img-responsive row" style="background-image: url('http://manage.band/images/merchshot.jpg');background-repeat:no-repeat;max-height:500px;">
                     <div class="col-sm-6 pull-left" style="background-color:#000;color:#fff;height:500px;padding-top:15%;opacity:.8;font-size:1em">
                         Sell tickets & merch online, inventory management and shipping only takes a few clicks.<br>
                         Our transaction fee is only 6% of the whole order with no charge for listing.<br> <br>
