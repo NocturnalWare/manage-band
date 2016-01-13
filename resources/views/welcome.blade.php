1b4ddf4e684511760c93c890b87e60b5ad81c69b
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-12 23:21:41 -0500

    Fixes bottom margin. Encapsulates images.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.front')
 @section('content')
-<div class="" style="margin-bottom:50px;">
+<div style="margin-bottom:50px;">
     <div class="col-md-1"></div>
     <div class="col-sm-12 col-md-10" style=";border-radius:10px;">
         <div class="container col-md-8" style="padding:10px;border-radius:10px;">
@@ -43,31 +43,37 @@
             </form>
         </div>
 
-        <div class=" col-sm-12" style="font-size:1.5em;border-radius:10px;">
+        <div class=" col-sm-12" style="font-size:1.5em;border-radius:10px;margin-bottom:90px;">
             <div class="col-sm-12">
-                <div class="img-responsive col-xs-12 pull-left row" style="padding-left:3%;background-image: url('http://manage.band/images/groupshot.jpg');background-repeat:no-repeat;height:500px;width:100%">
+                <div class="img-responsive col-xs-12 pull-left row" style="border-radius:10px;padding-left:3%;background-image: url('http://manage.band/images/groupshot.jpg');background-repeat:no-repeat;height:450px;width:100%">
                     <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
                         Manage your business from the studio, the road, or anywhere else that has an internet connection. <br> <br>
                     </div>
                 </div>
             </div>
 
-            <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/frontshot.jpg');background-repeat:no-repeat;height:500px;width:100%">
-                <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
-                    From amps to Zildjian, manage your gear and make sure you have everything you need to succeed. <br> <br>
+            <div class="col-sm-12">
+                <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/frontshot.jpg');background-repeat:no-repeat;height:450px;width:100%">
+                    <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
+                        From amps to Zildjian, manage your gear and make sure you have everything you need to succeed. <br> <br>
+                    </div>
                 </div>
             </div>
 
-            <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/showshot.jpg');background-repeat:no-repeat;height:500px;width:100%">
-                <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
-                    Connect with the people you need and social media to quickly set up a show or tour. <br> <br>
+            <div class="col-sm-12">
+                <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/showshot.jpg');background-repeat:no-repeat;height:450px;width:100%">
+                    <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
+                        Connect with the people you need and social media to quickly set up a show or tour. <br> <br>
+                    </div>
                 </div>
             </div>
 
-            <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/merchshot.jpg');background-repeat:no-repeat;height:500px;width:100%">
-                <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
-                    Sell tickets & merch online, inventory management and shipping only takes a few clicks.<br><br>
-                    Our transaction fee is only 7% of the whole order with no charge for listing.<br> <br>
+            <div class="col-sm-12">
+                <div class="img-responsive col-xs-12 pull-left row" style="background-image: url('http://manage.band/images/merchshot.jpg');background-repeat:no-repeat;height:450px;width:100%">
+                    <div class="col-xs-12 col-md-4 pull-right" style="background-color:#000;color:#fff;height:400px;padding-top:9%;opacity:.8;border-radius:15px;font-size:1em">
+                        Sell tickets & merch online, inventory management and shipping only takes a few clicks.<br><br>
+                        Our transaction fee is only 7% of the whole order with no charge for listing.<br> <br>
+                    </div>
                 </div>
             </div>
         </div>
