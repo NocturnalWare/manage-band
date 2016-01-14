ebd108b871b60ad9ce59f18785635e3ff5cf49ef
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-14 02:15:44 -0500

    fixes menu icon

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -22,7 +22,7 @@
 <nav class="navbar">
   <div class="container-fluid" style="background-color:#404040;color:#fff">
     <!-- Brand and toggle get grouped for better mobile display -->
-    <div class="navbar-header" >
+    <div class="navbar-header"  style="background-color:#404040;color:#fff">
       <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
         <span class="sr-only">Toggle navigation</span>
         <span class="icon-bar"></span>
