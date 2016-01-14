4c8a038bc20cf65a16e3e246d77f135366e7b654
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-14 00:26:00 -0500

    updates language

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -25,8 +25,8 @@
                         </div>
                         <div class="row" style="display:flex;min-height:600px;background-image: url('http://manage.band/images/connect.jpg');background-repeat:no-repeat;background-size:100%;"></div>
                         <div class="row" style="background-color:#000;color:#fff;padding-top:9%;border-radius:15px;text-align:center">
-                            Sell tickets & merch online, manage inventory and shipping with only takes a few clicks.<br><br>
-                            Our transaction fee is only 7% of the whole order with no other charges.<br> <br>
+                            Sell tickets & merch online, manage inventory and shipping with only a few clicks.<br><br>
+                            Our transaction fee is only 7% of the whole order -- no other charges.<br> <br>
                             <button type="button" class="btn btn-default btn-lg" style="width:50%;height:100px" data-toggle="modal" data-target="#myModal">
                                 Sign Up! <i class="fa fa-arrow-right"></i>
                             </button>
