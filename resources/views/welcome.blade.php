d10554d92ec32a15e4d724596fbc3486358483f3
Jota Vares <joe@eternallynocturnal.com.com>
2016-01-14 00:42:47 -0500

    maybe fixes image aspect ratio

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,29 +6,32 @@
         <div class="row">
             <div class="row">
 
-                    <div class="row" style="background-color:#000;color:#fff;padding:2%;opacity:.8;border-radius:15px;font-size:4em">
+                    <div class="row" style="background-color:#000;color:#fff;padding:2%;opacity:.8;border-radius:15px;font-size:3em">
 
                         <div class="row" style="min-height:700px;background-image: url('http://manage.band/images/n64.jpg');background-repeat:no-repeat;background-size:100%;">
-                        <div class="row" style="background-color:#000;color:#fff;padding:2%;opacity:.8;border-radius:15px;text-align:center">
-                            Manage your business from the studio, the road, or anywhere else that has an internet connection. <br> <br>
-                        </div>
+                            <div class="col-sm-12" style="background-color:#000;color:#fff;padding:2%;opacity:.8;border-radius:15px;text-align:center">
+                                Manage your business from the studio, the road, or anywhere else that has an internet connection. <br> <br>
+                            </div>
                         <!-- spacer -->
-                        <div class="row" style="min-height:400px"></div>
-
-                        <div class="row" style="background-color:#000;color:#fff;opacity:.8;border-radius:15px;text-align:center">
-                                From amps to Zildjian, manage your gear and make sure you have everything you need to succeed. <br> <br>
-                            <div class="row" style="margin-left:2%;min-height:600px;background-image: url('http://manage.band/images/brennandrums2.jpg');background-repeat:no-repeat;background-size:99%;"></div>
+                            <div class="row" style="min-height:400px"></div>
+                        <div class="row" style="opacity:.2;min-height:600px;background-image: url('http://manage.band/images/brennandrums2.jpg');background-repeat:no-repeat;background-size:100%;">
+                            <div class="col-sm-12" style="background-color:#000;color:#fff;opacity:.8;border-radius:15px;text-align:center">
+                                    From amps to Zildjian, manage your gear and make sure you have everything you need to succeed.
+                            </div>
+                            <div class="row" style="min-height:600px"></div>
+                        </div>
                         </div>
 
-                        <div class="row" style="background-color:#000;color:#fff;opacity:.8;padding-top:9%;border-radius:15px;text-align:center">
+                        <div class="col-sm-12" style="background-color:#000;color:#fff;opacity:.8;padding-top:9%;border-radius:15px;text-align:center">
                                 Connect with social media and the people you need to manage a show or tour. <br> <br>
-                        </div>
                         <div class="row" style="display:flex;min-height:600px;background-image: url('http://manage.band/images/connect.jpg');background-repeat:no-repeat;background-size:100%;"></div>
-                        <div class="row" style="background-color:#000;color:#fff;padding-top:9%;border-radius:15px;text-align:center">
+                        </div>
+                        
+                        <div class="col-sm-12" style="background-color:#000;color:#fff;padding-top:9%;border-radius:15px;text-align:center">
                             Sell tickets & merch online, manage inventory and shipping with only a few clicks.<br><br>
                             <button type="button" class="btn btn-default btn-lg" style="width:50%;height:100px" data-toggle="modal" data-target="#myModal">
                                 Sign Up for Free! <i class="fa fa-arrow-right"></i>
-                            </button>
+                            </button><br><br>
                             Our transaction fee is only 7% of the whole order -- no other charges.<br> <br>
                         </div>
                     </div>
